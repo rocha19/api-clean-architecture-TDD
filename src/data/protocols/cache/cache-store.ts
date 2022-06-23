f341d3f1fe9eb0a997a86989f1ce3a2ad8609f95
rocha19 <rochaarqueologia@outlook.com>
2022-06-23 10:04:46 -0300

    feat: ensure LocalSabePurchases insert correct data on cache

--- src/data/protocols/cache/cache-store.ts
+++ src/data/protocols/cache/cache-store.ts
@@ -1,8 +1,8 @@
-export interface CacheStore {
-  delete: (key: string) => void
-  insert(insertKey: string): void
-}
 // export interface CacheStore<T> {
 //   delete(key: string): void
 //   insert(key: string, value: T): void
 // }
+export interface CacheStore {
+  delete: (key: string) => void
+  insert(insertKey: string, value: any): void
+}
--- src/data/use-cases/save-purchases/local-save-purchases.ts
+++ src/data/use-cases/save-purchases/local-save-purchases.ts
@@ -1,9 +1,10 @@
 import { CacheStore } from "@/data/protocols/cache";
+import { SavePurchases } from "@/domain/use-cases";
 
-export class LocalSabePurchases {
+export class LocalSabePurchases implements SavePurchases {
   constructor(private readonly cacheStore: CacheStore) {}
-  async save(): Promise<void> {
+  async save(purchases: Array<SavePurchases.Params>): Promise<void> {
     this.cacheStore.delete('purchases')
-    this.cacheStore.insert('purchases')
+    this.cacheStore.insert('purchases', purchases)
   }
 }
