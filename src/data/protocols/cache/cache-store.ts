c2b0b1eaa2e8c81e810a11dcf4672e9cc5e6adc2
rocha19 <rochaarqueologia@outlook.com>
2022-06-23 09:26:08 -0300

    fix: delete useless folder

--- src/data/protocols/cache/cache-store.ts
+++ src/data/protocols/cache/cache-store.ts
@@ -1,3 +1,7 @@
 export interface CacheStore {
   delete: (key: string) => void
 }
+// export interface CacheStore<T> {
+//   delete(key: string): void
+//   insert(key: string, value: T): void
+// }
--- src/domain/data/use-cases/save-purchases/local-save-purchases.ts
+++
@@ -1,8 +0,0 @@
-import { CacheStore } from '@/data/protocols/cache'
-
-export class LocalSabePurchases {
-  constructor(private readonly cacheStore: CacheStore) {}
-  async save(): Promise<void> {
-    this.cacheStore.delete('purchases')
-  }
-}
